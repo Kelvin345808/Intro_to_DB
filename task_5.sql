138f90d8b4eda60ba571a5d07bfb861a9cf8ae4e
kelvin nzau <kelvinnzau10@gmail.com>
2025-02-02 19:11:48 +0300

    task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,6 @@
 -- Select the alx_book_store database (passed as an argument in mysql command)
 USE alx_book_store;
 
--- Insert a single row into the customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
+-- Insert a single row into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
